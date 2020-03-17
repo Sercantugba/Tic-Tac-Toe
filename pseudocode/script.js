b444015eb3b4b227ca5df3a16b4068ba8dceaacb
Sercan <Student_M21@admins-air-5.home>
2020-03-16 20:03:35 -0500

    pseudocode folder

---
+++ pseudocode/script.js
@@ -0,0 +1,50 @@
+// Pseudocode for TicTac Toe
+
+/* Before a play starts playing, the game grid will be set to null (Which means no-value).
+The first player input will be "X" which moves the array to 0, 
+while the second player which is the computer input is "O" and it moves the array to move to increment. */
+
+
+/* First I defined a variable called build which is an object. 
+Inside this variable would be all the javascript functions which will perform the player and computer inputs. 
+Next I created the build.ticTacToe, inside this array would be the game status.
+Our build.ticTacToe will have 9 inputs which will hold each box in the game grid, which will range from "0-8" based on javascript array concept. */
+
+
+// Will be two player
+// Player 1(sercan) and Computer
+// Game 3x3 grid boxes
+// There are 8 chance to win
+// horizontally : 3
+// vertically : 3
+// diagonally : 2
+
+// used an array function to find winner
+// if player1 or computer pick three symbols in the same line will be winner.
+// else will be no winner
+
+// Resources used
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+
+// https://stackoverflow.com/questions/14422898/how-to-get-an-elements-id-from-event-target/14422901
+
+// https://www.w3schools.com/jsref/event_target.asp
+
+// https://api.jquery.com/
+
+// https://www.w3schools.com/jquery/
+
+// https://api.jquery.com/category/events/
+
+// https://www.w3schools.com/jsref/met_node_appendchild.asp
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
+
+// https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Grids
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLOrForeignElement/dataset
+
+// https://www.w3schools.com/jsref/jsref_indexof.asp
